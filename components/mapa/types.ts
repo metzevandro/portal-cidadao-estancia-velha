b5e64d104b1093121d7cb3e191f9799daf93f187
metzevandro <metzxevandro@gmail.com>
2025-11-08 21:26:37 -0300

    fix: adicionado ao type o tipo de solicitação e adicionado mais fatores para o endereco da solicitação

--- components/mapa/types.ts
+++ components/mapa/types.ts
@@ -1,8 +1,19 @@
+export type TipoSolicitacao = {
+  id: string;
+  nome: string;
+};
+
 export type Solicitacao = {
+  id: string;
+  userId: string;
   lat: number;
   lng: number;
   tipo: string;
   descricao: string;
-  imageUrl?: string;
+  imageUrl: string;
   bairro: string;
+  rua: string;
+  numero: string;
+  cep: string;
+  status: "pendente" | "em_analise" | "concluida" | "rejeitada";
 };
